a3455801eee9ef1cf9bcee7806faa74db98df132
sttinh99 <truongtinh110399@gmail.com>
2021-06-07 00:22:46 +0700

    ab

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/', (req, res) => {
 })
 
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT
 http.listen(PORT, () => {
     console.log('server is running on port', PORT);
 })
