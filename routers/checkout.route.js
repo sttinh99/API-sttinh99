6b97aea312b4547f3699f785233de6a6ac12b485
sttinh99 <truongtinh110399@gmail.com>
2021-06-28 08:42:52 +0700

    checkout

--- routers/checkout.route.js
+++ routers/checkout.route.js
@@ -3,7 +3,7 @@ const checkoutController = require('../controllers/checkout.controller');
 const auth = require('../middleware/auth.middleware');
 
 
-router.get('/', auth.auth, checkoutController.getCheckout);
+router.get('/', checkoutController.getCheckout);
 router.post('/', auth.auth, checkoutController.createCheckout);
 router.post('/:id', auth.auth, checkoutController.updateCheckout);
 router.delete('/delete/:id', auth.auth, checkoutController.deleteCheckout)
